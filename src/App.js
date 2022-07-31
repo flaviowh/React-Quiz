03896aabfb2778464a89332be64b184d575eba7e
Flávio <flavioemsc@gmail.com>
2022-07-31 14:54:48 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from "react"
-import Question from './Question.js';
+import Questions from './Questions.js';
 import { nanoid } from 'nanoid'
 
 
@@ -17,7 +17,6 @@ export default function App() {
   }, [])
 
   const seedata = () => console.log(questions)
-
 
   function formatData(data) {
 
@@ -48,44 +47,19 @@ export default function App() {
 
   const [started, setStarted] = React.useState(false)
 
-  function mixAnswers(question) {
-    let answers = [question.correct_answer].concat(question.incorrect_answers)
-    answers = answers.sort(() => Math.random() - 0.5)
-    return answers.map(ans => { return { "text": ans, "selected": false } })
-  }
-
-
-  const questionsList = questions.map(qst => (
-    <Question
-      key={qst.id}
-      question={qst.question}
-      answers={mixAnswers(qst)}
-    />
-  ))
-
-  function questionsDiv() {
-    return (
-      <div className='quizPage'>
-        <h1 className='page-title'>React Quiz</h1>
-        {questionsList}
-        <button className='check-btn'>Check Answers</button>
-      </div>
-    )
-  }
-
   function welcomeDiv() {
     return (
       <div className='welcome'>
         <h1 className='app-name'>React Quiz</h1>
         <h3 className='app-description'>Can you answer these questions?</h3>
-        <button className='start-btn' onClick={()=> setStarted(true)} >Start Quiz</button>
+        <button className='start-btn' onClick={() => setStarted(true)} >Start Quiz</button>
       </div>
     )
   }
 
   return (
     <div className="App">
-      {started ? questionsDiv() : welcomeDiv()}
+      {started ? <Questions questions={questions}/> : welcomeDiv()}
     </div>
   );
 }
